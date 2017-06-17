26a313e25302f4946977d7fb586f6fbf3248da1a
Sumi <mugan27@gmail.com>
2017-06-17 10:19:21 +0200

    template integration in progress

--- Controllers/FrontIkinoaController.php
+++ Controllers/FrontIkinoaController.php
@@ -16,12 +16,32 @@ class FrontIkinoaController extends Controller
     /**
      * @return array
      */
+    public function index()
+    {
+        $response = $this->getThemes(3);
+        $response['price'] = $this->app->data['setting']['payment'];
+        unset($response['price']['stripe']);
+        return $response;
+    }
+
+    /**
+     * @return array
+     */
     public function theme()
+    {
+        return $this->getThemes();
+    }
+
+    /**
+     * @param null $max
+     * @return array
+     */
+    private function getThemes($max = null)
     {
         $domain = (isset($this->app->data['setting']['domain'])) ? $this->app->data['setting']['domain'] : '';
         $path = $domain . WEBROOT . 'site/';
         $professions = Profession::select('name', 'slug')->where('slug', 'IN', ['sport'])->get();
-        $themes = Theme::repo()->frontList(['sport']);
+        $themes = Theme::repo()->frontList(['sport'], $max);
         return compact('themes', 'professions', 'path');
     }
 
--- routes.php
+++ routes.php
@@ -4,10 +4,16 @@ return [
 
     /* Front */
 
+    '/module/ikinoa'	=> [
+        'use' => 'FrontIkinoaController@index',
+        'name' => 'ikinoa.page',
+        'template' => '/Page/index',
+    ],
+
     '/module/ikinoa/themes'	=> [
         'use' => 'FrontIkinoaController@theme',
-        'name' => 'ikinoa.registration.theme',
-        'template' => '/Registration/theme'
+        'name' => 'ikinoa.page.theme',
+        'template' => '/Page/theme'
     ]
 
 ];
